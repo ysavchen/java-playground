b6efc82e919358c061ff3a8d3dc51d3b9772f2ee
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-25 18:13:17 +0300

    Add main class

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    kotlin("jvm") version "2.2.21"
+}
+
+group = "org.example"
+version = "1.0.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/example/MainJava.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MainJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
