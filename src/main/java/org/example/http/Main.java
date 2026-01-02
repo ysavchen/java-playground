766dfc0cd238b616030defaec97d0a653a1bd1c6
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-02 15:04:17 +0300

    Add http directory

---
+++ src/main/java/org/example/http/Main.java
@@ -0,0 +1,20 @@
+package org.example.http;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://www.google.com/"))
+            .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+            .thenApply(HttpResponse::body)
+            .thenAccept(System.out::println)
+            .join();
+    }
+}
