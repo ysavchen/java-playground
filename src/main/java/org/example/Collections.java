c60072134fdf74dc1196300f07195be1459af7ce
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-05 20:13:30 +0300

    Update collections

--- src/main/java/org/example/Collections.java
+++ src/main/java/org/example/Collections.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import java.util.*;
+
+import static java.util.Collections.reverseOrder;
+
 public class Collections {
 
     public static void main(String[] args) {
+        arrayDeque();
+        priorityQueue();
+        hashMap();
+    }
 
+    private static void arrayDeque() {
+        Deque<String> queue = new ArrayDeque<>();
+        queue.removeLast();
+        String result = queue.pop();
+        System.out.println(result);
+    }
+
+    private static void priorityQueue() {
+        PriorityQueue<Integer> integerQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> reversedQueue = new PriorityQueue<>(reverseOrder());
+    }
+
+    private static void hashMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "blabla");
+        String result = map.get("1");
+        System.out.println(result);
     }
 }
