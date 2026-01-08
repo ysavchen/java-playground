887d371e2e3b63b1e5c222dcc25c5ddb096262cf
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-08 15:22:33 +0300

    Remove examples

--- src/main/java/org/example/UtilsExample.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class UtilsExample {
-
-    public static void main(String[] args) {
-        //Objects, Arrays, Math, Collections (emptyList(), reverseOrder(), binarySearch())
-    }
-}
