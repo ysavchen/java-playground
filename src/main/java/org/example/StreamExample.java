95987509d581df750180748110e5e4bba7bf331b
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-28 15:46:27 +0300

    Add examples

---
+++ src/main/java/org/example/StreamExample.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        distinctWords();
+    }
+
+    /**
+     * Разбить текст на уникальные слова
+     */
+    private static void distinctWords() {
+        String text = """
+            """;
+
+        String[] words = text.lines()
+            .filter(line -> !line.isEmpty())
+            .map(line -> Arrays.stream(line.split("\\s+")).toList())
+            .flatMap(Collection::stream)
+            .distinct()
+            .toArray(String[]::new);
+
+        System.out.println(Arrays.toString(words));
+    }
+}
