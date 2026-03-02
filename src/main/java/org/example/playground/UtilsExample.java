4b8c594cddeac8075d58ff5f00a2e66028ed248d
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-02 21:50:37 +0300

    Update utils example

--- src/main/java/org/example/playground/UtilsExample.java
+++ src/main/java/org/example/playground/UtilsExample.java
@@ -1,8 +1,23 @@
 package org.example.playground;
 
+import java.util.Arrays;
+
 public class UtilsExample {
 
     public static void main(String[] args) {
         //Objects, Arrays, Math, Collections (emptyList(), reverseOrder(), binarySearch())
+
+        // *** Arrays ***
+        String[] array = {"Anna", "Maria", "Elena"};
+        String[] secondArray = {"Elena", "Maria", "Anna"};
+
+        boolean equal = Arrays.equals(array, secondArray);
+        System.out.println(equal);
+
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        String[] copy = array.clone();
+        System.out.println(Arrays.toString(copy));
     }
 }
