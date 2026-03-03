262f85408c5fe755a503a1f60ffa6c98d2b65993
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-03 13:44:09 +0300

    Update UtilsExample

--- src/main/java/org/example/playground/UtilsExample.java
+++ src/main/java/org/example/playground/UtilsExample.java
@@ -7,7 +7,10 @@ public class UtilsExample {
     public static void main(String[] args) {
         //Objects, Arrays, Math, Collections (emptyList(), reverseOrder(), binarySearch())
 
-        // *** Arrays ***
+        math();
+    }
+
+    public static void arrays() {
         String[] array = {"Anna", "Maria", "Elena"};
         String[] secondArray = {"Elena", "Maria", "Anna"};
 
@@ -24,5 +27,15 @@ public class UtilsExample {
         int[][] tableCopy = table.clone();
         System.out.println(Arrays.deepEquals(table, tableCopy));
         System.out.println(Arrays.deepToString(tableCopy));
+    }
+
+    public static void math() {
+        double a = Math.pow(2, 3);
+        System.out.println("a: " + a);
+
+        int b = Math.abs(3);
+        int c = Math.abs(-4);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
     }
 }
