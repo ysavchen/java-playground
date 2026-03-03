a262c28c375b18e896dbf97c00fea5f6218ed800
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-03 14:38:11 +0300

    Update ControlFlowExample

--- src/main/java/org/example/playground/ControlFlowExample.java
+++ src/main/java/org/example/playground/ControlFlowExample.java
@@ -43,6 +43,11 @@ public class ControlFlowExample {
             System.out.println(i);
         }
 
+        // в инициализаторе можно объявить несколько переменных
+        for (int i = 0, j = 10; i < j; i++, j--) {
+            System.out.println("i = " + i + "; j = " + j);
+        }
+
         // *** for-each ***
         int[] array = {1, 2, 3, 4, 5};
         for (int i : array) {
